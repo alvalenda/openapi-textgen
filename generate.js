2261f959a6ff4862631d6c33c4b7c747fc41d677
alvalenda <banysan@msn.com>
2022-12-19 18:10:46 -0300

    gera textos pequenos

--- generate.js
+++ generate.js
@@ -1,17 +1,40 @@
 const { Configuration, OpenAIApi } = require("openai");
 
+const fs = require("fs");
+require("dotenv").config();
+
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-const response = await openai.createCompletion({
-  model: "text-davinci-003",
-  prompt:
-    "Topic: Breakfast\nTwo-Sentence Horror Story: He always stops crying when I pour the milk on his cereal. I just have to remember not to let him see his face on the carton.\n    \nTopic: Wind\nTwo-Sentence Horror Story:",
-  temperature: 0.8,
-  max_tokens: 60,
-  top_p: 1.0,
-  frequency_penalty: 0.5,
-  presence_penalty: 0.0,
-});
+async function generate() {
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt:
+      "Topic: História de um Personagem\nHistória em três setenças: Um ladino especializado em enfrentar usuários de magia que viveu até sua vida adulta sob lavagem cerebral de um culto religoso de Shar até ser liberto por sacerdotisas de Selune.",
+    temperature: 0.8,
+    max_tokens: 60,
+    top_p: 1.0,
+    frequency_penalty: 0.5,
+    presence_penalty: 0.0,
+  });
+
+  const data = response.data;
+  const texto = data.choices.reduce(
+    (acc, choice) => acc + " " + choice.text,
+    ""
+  );
+
+  saveToTextFile(texto);
+}
+
+// função salva valor de variável em arquivo de text
+function saveToTextFile(dataText) {
+  fs.writeFileSync("./public/texto.txt", dataText, (err) => {
+    if (err) return console.log(err);
+    console.log("Texto > ./public/texto.txt");
+  });
+}
+
+generate();
