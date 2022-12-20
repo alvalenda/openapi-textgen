56ed09d159ae0034b2a9bd9644007bea67d2f83a
alvalenda <banysan@msn.com>
2022-12-20 04:29:55 -0300

    chore(app): remove javascript files

--- initial-text.js
+++
@@ -1,5 +0,0 @@
-// This file is used to set the initial text of the editor.
-const initialText =
-  "How Heather fights depends on where the heroes encounter her. Within the circle shrine's area of effect, she uses her sickle +1 or shapechanges into her dire wolf form. Outside the circle's effect, she uses her spells. A favorite trick is to entangle a heavily armored fighter, and then cast heat metal on his armor. Another is to summon insects on someone caught in a snare. She also enjoys charming an opponent's war dog into attacking him.\nShe can be reduced to zero hit points, but Heather's gaseous form floats up to any tree hollow where she rests and regenerates in bat form. It takes 24 hours for her to recover to full strength.";
-
-module.exports = { initialText };
