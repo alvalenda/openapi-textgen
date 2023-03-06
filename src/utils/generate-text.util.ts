354b74f912ba66ba7780d7061bacebac2a182718
Flavio Alvarenga <flavio.alva@outlook.com>
2023-03-06 08:40:01 -0300

    chore(app): ts-node

--- src/utils/generate-text.util.ts
+++ src/utils/generate-text.util.ts
@@ -1,6 +1,6 @@
 // this sets the instructions for openAI generation
 export const instructions: string =
-  "Tópico: Gerar um score que reflita a emoção da frase\n Emoções: frustrated, angry, tense, excited, delighted, happy, content, relaxed, calm, tired, bored, depressed\n Escala: 0.0 a 1.0\n Modelo: Circumplex model\n Formato: JSON{patientText: '', emotions: { emotion: value, emotion: value ... }}";
+  "Tópico: Gerar um score que reflita a emoção da frase\n Emoções: frustrated, angry, tense, excited, delighted, happy, content, relaxed, calm, tired, bored, depressed\n Escala: 0.0 a 1.0 (Só se aproxime de 1.0 em casos muito extremados)\n Modelo: Circumplex model\n Formato: JSON{patientText: '', emotions: { emotion: value, emotion: value ... }}";
 
 // This file is used to set the initial text of the editor.
 export const patientText: string =
